55025a89f4084be1e454dc13f5494ec7f4bd5c70
William Wilhelm <will@wilhelm.com.au>
2017-06-24 15:36:58 +1000

    Update JSON parser syntax

--- Myth/Status.hs
+++ Myth/Status.hs
@@ -3,28 +3,21 @@
 module Myth.Status where
 import Network.HTTP.Simple
 import Network.HTTP.Client
-import Data.Aeson (Value)
 import Data.Aeson.Types
 import qualified Data.Vector as V
 import Data.Bits.Bitwise
 
-parseEncoder :: Value -> Parser String
-parseEncoder = withObject "Encoder" $ \o -> do
-    enc <- pure o >>= (.: "Connected")
-    return enc
+parseStatus = withObject "EncoderList" $ \o -> do
+    statuses <- pure o >>= (.: "EncoderList") >>= (.: "Encoders") >>= (mapM (.: "Connected")) . (V.toList)
+    return . fromListLE $ (== ("true" :: String)) <$> statuses
 
-parseEncoders :: Value -> Parser Int
-parseEncoders = withObject "EncoderList" $ \o -> do
-    encs <- pure o >>= (.: "EncoderList") >>= (.: "Encoders")
-    let Success statuses = parse (withArray "Encoders" $ \a -> mapM parseEncoder (V.toList a)) encs
-    return . fromListLE $ (== "true") <$> statuses
-
+getStatusCode :: IO Int
 getStatusCode = do
     req <- parseRequest "http://worker1:6544/Dvr/GetEncoderList" >>= \req -> return req { requestHeaders = [("Accept", "application/json")] }
 
-    res <- getResponseBody <$> httpJSON req :: IO Value
-    
-    let Success num = parse parseEncoders res
+    res <- getResponseBody <$> httpJSON req
+
+    let Success num = parse parseStatus res
 
     return num
 
