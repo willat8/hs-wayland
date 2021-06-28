995f6c23aefd80e4fc7ebcc3d86c7ebdae6406be
William Wilhelm <willat8@gmail.com>
2021-06-28 20:35:42 +1000

    Display the number of unreachable light bulbs

--- Myth/Render.hs
+++ Myth/Render.hs
@@ -1,7 +1,7 @@
 module Myth.Render (drawStatus, drawClock, drawAlert) where
 import qualified Myth.Internal as M
 import Myth.Alert
-import Control.Monad (zipWithM_)
+import Control.Monad (zipWithM_, when)
 import Data.Bits.Bitwise
 import qualified Data.ByteString as S
 import qualified Data.ByteString.Internal as B
@@ -147,7 +147,6 @@ drawMarquee time = do
 
 drawDashboard babyMonitorHealth isHDHomeRunHealthy isMythTVHealthy isPiholeHealthy hueHealth = do
     let isMicrophoneUnhealthy:isSpeakerUnhealthy:_ = toListLE babyMonitorHealth
-    let isHueHealthy = hueHealth == healthy
     drawBlank
     setOperator OperatorOver
     scale 0.1 0.1
@@ -167,7 +166,13 @@ drawDashboard babyMonitorHealth isHDHomeRunHealthy isMythTVHealthy isPiholeHealt
     translate 1400 0
     drawRaspberryPiIcon $ if (isPiholeHealthy) then "green" else "red"
     translate 1400 0
-    drawLightbulbIcon $ if (isHueHealthy) then "green" else "red"
+    drawLightbulbIcon $ if (hueHealth == healthy) then "green" else "red"
+    identityMatrix
+    moveTo 700 25
+    setSourceRGBA 1 1 1 0.8
+    selectFontFace "sans-serif" FontSlantItalic FontWeightBold
+    setFontSize 24
+    when (hueHealth > 0) $ showText (show hueHealth) -- One of more lights are unreachable
     return ()
 
 drawBabyIcon colour = do svgRenderFromString ("<svg aria-hidden=\"true\" focusable=\"false\" data-prefix=\"fas\" data-icon=\"baby\" class=\"svg-inline--fa fa-baby fa-w-12\" role=\"img\" xmlns=\"http://www.w3.org/2000/svg\" viewBox=\"0 0 384 512\"><path fill=\"" ++ colour ++ "\" d=\"M192 160c44.2 0 80-35.8 80-80S236.2 0 192 0s-80 35.8-80 80 35.8 80 80 80zm-53.4 248.8l25.6-32-61.5-51.2L56.8 383c-11.4 14.2-11.7 34.4-.8 49l48 64c7.9 10.5 19.9 16 32 16 8.3 0 16.8-2.6 24-8 17.7-13.2 21.2-38.3 8-56l-29.4-39.2zm142.7-83.2l-61.5 51.2 25.6 32L216 448c-13.2 17.7-9.7 42.8 8 56 7.2 5.4 15.6 8 24 8 12.2 0 24.2-5.5 32-16l48-64c10.9-14.6 10.6-34.8-.8-49l-45.9-57.4zM376.7 145c-12.7-18.1-37.6-22.4-55.7-9.8l-40.6 28.5c-52.7 37-124.2 37-176.8 0L63 135.3C44.9 122.6 20 127 7.3 145-5.4 163.1-1 188 17 200.7l40.6 28.5c17 11.9 35.4 20.9 54.4 27.9V288h160v-30.8c19-7 37.4-16 54.4-27.9l40.6-28.5c18.1-12.8 22.4-37.7 9.7-55.8z\"></path></svg>")
