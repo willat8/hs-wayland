d6f4d9b82388b3a920b177d9908a9d9f5e5e010c
William Wilhelm <willat8@gmail.com>
2021-07-27 14:23:52 +1000

    Check how many days of EPG data we have

--- Myth/Alert.hs
+++ Myth/Alert.hs
@@ -10,6 +10,10 @@ import qualified Data.HashMap.Strict as HashMap
 import Network.HTTP.Simple
 import Network.HTTP.Client
 import Data.Aeson.Types
+import Data.Fixed (divMod')
+import Data.Maybe (listToMaybe)
+import Data.Time.Clock
+import Data.Time.LocalTime
 import qualified Data.Vector as V
 import Control.Exception (try)
 
@@ -36,18 +40,21 @@ getHDHomeRunStatus = do
 
     return status
 
-parseMythTVStatus = withObject "GetSettingList" $ \o ->
-    pure o >>= (.: "SettingList") >>= (.: "Settings") >>= (.: "mythfilldatabaseLastRunStatus") >>= return . ((== "Successful.") :: String -> Bool)
+parseMythTVStatus = withObject "GetProgramList" $ \o ->
+    pure o >>= (.: "ProgramList") >>= (.: "Programs") >>= (mapM (.: "EndTime")) . (V.toList) >>= mapM (return . zonedTimeToUTC) >>= return . listToMaybe
 
 getMythTVStatus = do
-    req <- parseRequest "http://rancher:6544/Myth/GetSettingList" >>= \req -> return req { requestHeaders = [("Accept", "application/json")], responseTimeout = Just 5000000 }
+    currentUtcTime <- getCurrentTime
+
+    req <- parseRequest "http://rancher:6544/Guide/GetProgramList?StartTime=2021-01-01T00:00:00&EndTime=2031-01-01T00:00:00&Count=1&Descending=1" >>= \req -> return req { requestHeaders = [("Accept", "application/json")], responseTimeout = Just 5000000 }
 
     eres <- try $ httpJSON req :: IO (Either HttpException (Response Value))
 
-    status <- case eres of Right res -> return isHealthy
+    status <- case eres of Right res -> return $ daysInEpg >= 5
                                         where body = getResponseBody res
-                                              isHealthy = case (parse parseMythTVStatus body) of Success result -> result
-                                                                                                 Error _        -> False
+                                              lastEpgDate    = case (parse parseMythTVStatus body) of Success (Just result) -> result
+                                                                                                      _                     -> currentUtcTime
+                                              (daysInEpg, _) = timeToDaysAndTimeOfDay (diffUTCTime lastEpgDate currentUtcTime)
                            _         -> return False
 
     return status
@@ -89,4 +96,12 @@ getHueStatus = do
 count "" _      = 0
 count search bs = if B.null t then 0 else 1 + count search (B.drop (B.length search) t)
     where (h,t) = B.breakSubstring search bs
+
+timeToDaysAndTimeOfDay :: NominalDiffTime -> (Integer, TimeOfDay)
+timeToDaysAndTimeOfDay dt =
+    let s = realToFrac dt
+        (m, ms) = divMod' s 60
+        (h, hm) = divMod' m 60
+        (d, dh) = divMod' h 24
+     in (d, TimeOfDay dh hm ms)
 
